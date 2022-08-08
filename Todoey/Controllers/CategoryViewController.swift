076537f744a78011225aae1eeaa5d4f105611289
matthiasvanwoensel <matthias@fluvio.be>
2022-08-08 16:15:40 +0200

    categories

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,102 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Matthias Van Woensel on 08/08/2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    
+    // Core Data
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default){
+            (action) in
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+           
+            self.categories.append(newCategory)
+          
+            self.saveCategories()
+        
+        }
+        
+        alert.addTextField{(alertTextField) in
+            alertTextField.placeholder = "Add a new category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
+        return cell
+    }
+    
+    //MARK - Tableview DELEGATE Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    
+    }
+    
+   
+
+    func saveCategories(){
+        
+        do{
+            try context.save()
+        }catch{
+            print("Error saving context \(error)")
+        }
+        
+        tableView.reloadData()
+    
+    
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()){
+      
+  
+        do{
+            categories = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    
+    
+
+}
