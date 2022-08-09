e14f250fcf3b44cd2431438e603b1bf82ba601c3
matthiasvanwoensel <matthias@fluvio.be>
2022-08-09 10:09:39 +0200

    afwerking core data

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -12,7 +12,7 @@ import CoreData
 class CategoryViewController: UITableViewController {
     
     var categories = [Category]()
-    
+
     // Core Data
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
@@ -65,8 +65,16 @@ class CategoryViewController: UITableViewController {
     
     //MARK - Tableview DELEGATE Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        performSegue(withIdentifier: "goToItems", sender: self)
     
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
     }
     
    
